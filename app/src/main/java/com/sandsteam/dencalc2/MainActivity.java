6b6e76ef81e0e575656c756bbe50b638d8844d56
MYSTELTAiNN <mysteltainnofficial@users.noreply.github.com>
2021-05-17 23:25:54 +0700

    Update MainActivity.java as Splash Screen Update Layout activity_main.xml Add Empty Activity as Home Activity

--- app/src/main/java/com/sandsteam/dencalc2/MainActivity.java
+++ app/src/main/java/com/sandsteam/dencalc2/MainActivity.java
@@ -2,7 +2,9 @@ package com.sandsteam.dencalc2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i =new Intent(MainActivity.this, HomeActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+                finish();
+            }
+        }, 2500);
     }
 }
