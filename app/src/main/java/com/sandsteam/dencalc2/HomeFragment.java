05ab4e608a498076e25bfe08ae1aa2ecce868516
AverillSaladin03 <76471341+AverillSaladin03@users.noreply.github.com>
2021-05-31 13:06:46 +0700

    fix list barang layout+layout profile fragment

--- app/src/main/java/com/sandsteam/dencalc2/HomeFragment.java
+++ app/src/main/java/com/sandsteam/dencalc2/HomeFragment.java
@@ -7,58 +7,26 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private View home_view;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private Spinner frhome_spinerGolongan;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        home_view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        frhome_spinerGolongan = (Spinner) home_view.findViewById(R.id.frhome_spinerGolongan);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(HomeFragment.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.tesData_golongan));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        frhome_spinerGolongan.setAdapter(myAdapter);
+
+        return home_view;
     }
 }
