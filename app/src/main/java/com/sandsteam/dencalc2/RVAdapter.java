e9cd7559d02a0637b9e45b1482cdda24b8093ef5
MYSTELTAiNN <53290732+MYSTELTAiNNOfficial@users.noreply.github.com>
2021-06-01 12:55:35 +0700

    Add Process to Delete Item on API and RecyclerView

--- app/src/main/java/com/sandsteam/dencalc2/RVAdapter.java
+++ app/src/main/java/com/sandsteam/dencalc2/RVAdapter.java
@@ -1,22 +1,36 @@
 package com.sandsteam.dencalc2;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import AddOn.Barang;
+import AddOn.VolleyManage;
 
 public class RVAdapter extends RecyclerView.Adapter<RVAdapter.BarangViewHold> {
 
     private ArrayList<Barang> liistBarang;
+
+    private Context context;
 
     public RVAdapter(ArrayList<Barang> liistBarang) {
         this.liistBarang = liistBarang;
@@ -26,6 +40,7 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.BarangViewHold> {
     public BarangViewHold onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         View view =layoutInflater.inflate(R.layout.barang_cardview, parent, false);
+        context = parent.getContext();
         return new BarangViewHold(view);
     }
 
@@ -35,6 +50,7 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.BarangViewHold> {
         holder.cv_infoPemakaian.setText(String.valueOf(liistBarang.get(position).getTotal_pemakaian()));
         holder.cv_infoWatt.setText(String.valueOf(liistBarang.get(position).getWatt_barang()));
         holder.cv_kategoriBarang.setText(String.valueOf(liistBarang.get(position).getTipe_barang()));
+        holder.index = position;
     }
 
     @Override
@@ -45,6 +61,7 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.BarangViewHold> {
     public class BarangViewHold extends RecyclerView.ViewHolder {
         private TextView cv_infoWatt, cv_infoJumlah, cv_infoPemakaian, cv_kategoriBarang;
         private ImageView cv_editButton, cv_deleteButton;
+        private int index;
         public BarangViewHold(View itemView) {
             super(itemView);
             cv_infoWatt = itemView.findViewById(R.id.cv_infoWatt);
@@ -53,6 +70,40 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.BarangViewHold> {
             cv_kategoriBarang = itemView.findViewById(R.id.cv_kategoriBarang);
             cv_editButton = itemView.findViewById(R.id.cv_editButton);
             cv_deleteButton = itemView.findViewById(R.id.cv_deleteButton);
+            cv_deleteButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    final String id_item = String.valueOf(liistBarang.get(index).getId());
+                    deleteProcess(id_item, index);
+                }
+            });
         }
+    }
+    private void deleteProcess(String id_item, int index){
+        String url = "http://192.168.1.68/API/delete-item.php";
+        StringRequest req = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        liistBarang.remove(index);
+                        notifyItemRemoved(index);
+                        notifyItemRangeChanged(index, liistBarang.size());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        ){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> data = new HashMap<>();
+                data.put("id",id_item);
+                return data;
+            }
+        };
+        VolleyManage.getInstance(context).addToRequestQueue(req);
     }
 }
