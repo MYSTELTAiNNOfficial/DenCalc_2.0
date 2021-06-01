8c486a87860c542d1ceff30683f33ebf8c67627c
MYSTELTAiNN <53290732+MYSTELTAiNNOfficial@users.noreply.github.com>
2021-06-01 13:39:39 +0700

    Changes on Profile Fragment

--- app/src/main/java/com/sandsteam/dencalc2/ProfileFragment.java
+++ app/src/main/java/com/sandsteam/dencalc2/ProfileFragment.java
@@ -10,21 +10,36 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import AddOn.SharedPref;
+import AddOn.User;
+
 
 public class ProfileFragment extends Fragment {
     private View view;
     private TextView profile_nama;
     private Button profile_logoutButton;
+    private User user;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_profile, container, false);
         initView();
+        setListen();
         return view;
     }
     private void initView(){
         profile_nama = view.findViewById(R.id.profile_nama);
         profile_logoutButton = view.findViewById(R.id.profile_logoutButton);
+        user = SharedPref.getInstance(getActivity()).getUser();
+    }
+    private void setListen(){
+        profile_nama.setText(user.getUsername());
+        profile_logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPref.getInstance(getContext()).logout();
+            }
+        });
     }
 }
